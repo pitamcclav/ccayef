89236a4e7e57706a35b7584199f3888ad5177317
ssekitooleko.peter <pitamcclav@gmail.com>
2024-11-07 22:20:13 +0300

    fixed images and documents

---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+// Serve static files from the "public" directory
+app.use(express.static('images'));
+app.use(express.static('docs'));
+
+
+// Endpoint to get list of files in 'publications' and 'reports'
+app.get('/documents/:type', (req, res) => {
+    const docType = req.params.type;
+    const docPath = path.join(__dirname, 'docs', docType);
+
+    fs.readdir(docPath, (err, files) => {
+        if (err) {
+            return res.status(500).send('Unable to scan directory');
+        }
+        const fileList = files.map(file => ({
+            name: file,
+            url: `/docs/${docType}/${file}`,
+            type: path.extname(file).toLowerCase() // File extension for icons
+        }));
+        res.json(fileList);
+        console.log(fileList);
+    });
+});
+
+
+
+
+// Endpoint to get image filenames
+app.get('/images', (req, res) => {
+    const galleryDir = path.join(__dirname, 'images/gallery');
+    fs.readdir(galleryDir, (err, files) => {
+        if (err) {
+            return res.status(500).send('Unable to scan directory');
+        }
+        // Send the list of filenames
+        res.json(files);
+
+        console.log(files);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
